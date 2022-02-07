8149577bae28bfe3c23e60ec7f0a2dc2ef903d26
stone <46424030+SrSt0n3@users.noreply.github.com>
2022-02-06 21:42:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const config = require('./config.json')
+if(!config.cortes) return console.log('config.json mal configurado')
+if(!config.arquivos) return console.log('config.json mal configurado')
+if(!config.separados) return console.log('config.json mal configurado')
+if(!fs.existsSync(`./${config.arquivos}`)) return console.log('config.json mal configurado')
+if(!fs.existsSync(`./${config.separados}`)) return console.log('config.json mal configurado')
+const arquivos = config.arquivos
+const separados = config.separados
+
+console.log('---------------------------')
+console.log('|  SEPARADOR BY STONEDEV  |')
+console.log('---------------------------')
+
+
+fs.readdir(`./${arquivos}`, function(error, files) {
+    console.log(`Foi encontrado ${files.length} arquivos`)
+    var grupos = []
+    var corte = config.cortes
+    for (i = 0; i < files.length; i++) {
+        for (var i = 0; i < files.length; i = i + corte) {
+            grupos.push(files.slice(i, i + corte));
+        }
+    }
+    for (c = 0; c < grupos.length; c++) {
+        console.log(grupos)
+        for (i = 0; i < grupos[1].length; i++) {
+            console.log(grupos[1][i]);
+            if (fs.existsSync(`./${separados}/${c}`)) {
+                fs.rename(`./${arquivos}/${grupos[c][i]}`, `./${separados}/${c}/${grupos[c][i]}`, function(err) {
+                    if (err) throw err
+                    console.log('separado!')
+                })
+            } else {
+                fs.mkdirSync(`./${separados}/${c}`);
+                fs.rename(`./${arquivos}/${grupos[c][i]}`, `./${separados}/${c}/${grupos[c][i]}`, function(err) {
+                    if (err) throw err
+                    console.log('separado!')
+                })
+            }
+        }
+    }
+});
